67da84e0422692968cea0e21d9f7e4adcd2704ce
SubajananiSwaminathan <subajanani@gmail.com>
2024-08-19 20:10:45 +0530

    Organized Files & Added Styling

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 import Link from "next/link";
-import Navigation from "./components/Navigation";
 
 export default function Page() {
   return (
     <div>
-      <Navigation />
-      <h1>The Wild Oasis. Welcome to Paradise!</h1>;
+      <h1>The Wild Oasis. Welcome to Paradise!</h1>
       <Link href="/cabins">Explore Luxury Cabins</Link>
     </div>
   );
