e357a04022bd1b504de25426bb644577d164c2fa
SubajananiSwaminathan <subajanani@gmail.com>
2024-08-21 20:05:05 +0530

    Updated Code

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 
 import CabinList from "../_components/CabinList";
+import Filter from "../_components/Filter";
 import Spinner from "../_components/Spinner";
 
 export const revalidate = 3600;
@@ -9,7 +10,9 @@ export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,8 +25,11 @@ export default async function Page() {
         hot tub under the stars. Enjoy nature&apos;s beauty in your own little
         home away from home. The perfect spot for a peaceful, calm vacation.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
